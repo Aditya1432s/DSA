96d8a29a83c4ac85996b9b41d40bae77485d74db
Aditya Chauhan <siddsingh1432@gmail.com>
2026-09-19 23:20:19 +0530

    Time: 0 ms (100%), Space: 7.8 MB (68.85%) - LeetHub

--- 1401-circle-and-rectangle-overlapping/1401-circle-and-rectangle-overlapping.cpp
+++ 1401-circle-and-rectangle-overlapping/1401-circle-and-rectangle-overlapping.cpp
@@ -1,17 +1,27 @@
 class Solution {
 public:
-    bool checkOverlap(int radius, int xCenter, int yCenter,
+    bool checkOverlap(int radius, int xcenter, int ycenter,
                       int x1, int y1, int x2, int y2) {
 
-        // Find the closest point of rectangle to circle center
-        int closestX = max(x1, min(xCenter, x2));
-        int closestY = max(y1, min(yCenter, y2));
+        int xi;
+        int yi;                
 
-        // Distance between circle center and closest point
-        int dx = closestX - xCenter;
-        int dy = closestY - yCenter;
+       if(x1 > xcenter){
+         xi = x1;
+       }else if( x2 < xcenter){
+          xi = x2;
+       }else{
+         xi = xcenter;
+       }
 
+          if(y1 > ycenter){
+         yi = y1;
+       }else if( y2 < ycenter){
+          yi = y2;
+       }else{
+         yi = ycenter;
+       }
         // Check whether closest point lies inside circle
-        return dx * dx + dy * dy <= radius * radius;
+        return sqrt ((xi - xcenter)*(xi - xcenter) + (yi - ycenter)*(yi - ycenter)) <= radius;
     }
 };
